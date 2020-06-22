fcc16a31f2bf4823c271a29163cd388b2e963c14
Dice-K39 <daisukehashimoto39@gmail.com>
2020-06-22 06:25:17 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,37 @@ require 'active_support/inflector'
 
 class Song
 
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
 
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  ###### Class methods ######
+
+  #------ Making attr_accessors ------
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -24,41 +54,9 @@ class Song
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
-
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
-  end
-
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  end
-
-  def table_name_for_insert
-    self.class.table_name
-  end
-
-  def values_for_insert
-    values = []
-    self.class.column_names.each do |col_name|
-      values << "'#{send(col_name)}'" unless send(col_name).nil?
-    end
-    values.join(", ")
-  end
-
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-
+#------ Making attr_accessors ------
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
-
 end
-
-
-
